c7523b181c2396ac604f015d056478c6207a97e8
vagrant <vagrant@precise32.(none)>
2014-02-10 20:33:46 +0000

    Initial Commit

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
-var express = require('express');
-
+var express = require ("express")
+, path = require ('path')
 var app = express();
-var port = 8000;
-app.listen(port);
-console("Express app listening on port" + port);
 
-app.get('/', function(rec, res)){
-	res.send('Hello, welcome to CS1501');}
+require('./routes')(app);
+//Points Express to a folder where you keep static files
+// e.g. css or client side js files
+app.use(express.static(path.normalize(__dirname) + '/public'))
+
+app.use(express.errorHandler());
+
+app.configure(function(){
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+
+})
+
+//these 3 lines tell express that we are going to be rendering html files
+//held in the public directory which should be in the same diretory as this file
+
+app.set('views', path.normalize(__dirname) + '/public');
+app.set('view engine' , 'html');
+app.engine('html', require('ejs').renderFile);
+
+var port = 8000;
+app.listen(8000);
+console.log('App listening on port:'  + port);
